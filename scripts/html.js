a18a60ce5cc4b9ecec759948084b297358f121b2
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-12-02 10:09:57 +0530

    domElement object holds these dom strings

--- scripts/html.js
+++ scripts/html.js
@@ -1,3 +1,7 @@
-var signUpForm ='<form action="" method = "post" id ="signUpForm"><input type="text" name="username" value="" placeholder= "username"/><input type="password" name="password" value="" placeholder="password"/><input type="text" name="email" value="" placeholder="email"/></form>';
+var domElements = {
 
-var loginForm = '<form action="" method="post"><input type="text" name="username" value="" placeholder="username"/><input type="password" name="password" value="" placeholder="password"/></form>';
+ 'signUpForm' :'<form action="" method = "post" id ="signUpForm"><input type="text" name="username" value="" placeholder= "username"/><input type="password" name="password" value="" placeholder="password"/><input type="text" name="email" value="" placeholder="email"/></form>',
+
+'loginForm': '<form action="" method="post"><input type="text" name="username" value="" placeholder="username"/><input type="password" name="password" value="" placeholder="password"/></form>'
+
+};
