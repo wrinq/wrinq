2085b484e477e1c527b5e3f368c1710e5c64bbe2
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2014-08-31 02:09:11 +0530

    Got the validation working

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,15 @@
 $("#preview").click(function(e){
-    console.log("clicked");
+    var validation = $("#preview_validation");
+    validation.text("");
     var name = $("#name_tenant").val();
     var email = $("#email_tenant").val();
     var rent =$("#rent_tenant").val();
     if(name) $("#var_name").text(name);
     if(email) $("#var_email").text(email);
     if(rent) $("#var_rent").text(rent);
+
+    if(!name||!rent){
+	validation.text("It is important that you fill in the name and email fields for preview to work");
+    }
 
 });
