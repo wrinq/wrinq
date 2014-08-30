0c846f29d4aa49896d43397ba2de4304f1ce44a4
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2014-08-31 01:56:29 +0530

    Got preview working

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,10 @@
-$(".button").click(function(e){
-if(!e.target.id==="notify") return;
-// save the parameters to the mailchimp api
+$("#preview").click(function(e){
+    console.log("clicked");
+    var name = $("#name_tenant").val();
+    var email = $("#email_tenant").val();
+    var rent =$("#rent_tenant");
+    if(name) $("#var_name").text(name);
+    if(email) $("#var_email").text(email);
+    if(rent) $("#var_rent").text(rent);
+
 });
