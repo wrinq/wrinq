b7300c9c9a29445f9a528f52ee8c3c9331ea3c45
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2014-08-31 01:58:32 +0530

    Small typo

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ $("#preview").click(function(e){
     console.log("clicked");
     var name = $("#name_tenant").val();
     var email = $("#email_tenant").val();
-    var rent =$("#rent_tenant");
+    var rent =$("#rent_tenant").val();
     if(name) $("#var_name").text(name);
     if(email) $("#var_email").text(email);
     if(rent) $("#var_rent").text(rent);
