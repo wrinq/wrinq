bc18785fa14aaa1a37256d2dc4cb78f7ca3a89fc
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2015-03-19 12:48:05 +0530

    Created another ad

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,9 +1,9 @@
 function random_ad(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-var ads = ["come_out.html","chaos_for_tenancy.html"];
+var ads = ["come_out.html","chaos_for_tenancy.html","tired_of_maintaining_books.html"];
 
-var ad = ads[random_ad(0,1)];
+var ad = ads[random_ad(0,2)];
 
 $("#ad_div").load("/ads/"+ad,function(){
 });
